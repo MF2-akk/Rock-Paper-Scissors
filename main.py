0b86532b1d66dde04acd49d0d78d6287fb3f4e9c
MF2-akk <gleb15461@gmail.com>
2025-12-28 16:13:51 +0300

    Fixed bugs and added a message about rival selection

--- main.py
+++ main.py
@@ -6,36 +6,34 @@ def second_player() -> str:
     randomIndex = random.randint(0, 2)
     return objects[randomIndex]
 
-char =int(input("please, type 0(paper), 1(rock) or 2(scissors)"))
 
-if char > 2 or char < 0:
-   print("please, try again")
-   char = int(input("please, type 0(paper), 1(rock) pr 2(scissors)"))
-   if char >2 or char < 0:
-              quit()
+while True:
+    try:
+        char = int(input("please, type 0(paper), 1(rock) or 2(scissors"))
+        if 0 <= char <= 2:
+            break
+        else:
+            print("Invalid input. Please, enter 0, 1, or 2")
+    except ValueError:
+        print("Invalid input. Please enter a number")
 
 #-1 is lose, 0 is draw and 1 is win
 def rules(a, b) -> int:
-    if a == objects[0] and b == objects[1]:
-        return 1
-    if a == objects[0] and b == objects[2]:
-        return -1
-    if a == objects[0] and b == objects[0]:
+    if a == b:
         return 0
-    if a == objects[1] and b == objects[0]:
+    if (a == "paper" and b == "rock") or \
+       (a == "rock" and b == "scissors") or \
+       (a == "scissors" and b == "paper"):
+           return 1
+    else:
         return -1
-    if a == objects[1] and b == objects[2]:
-        return 1
-    if a == objects[1] and b ==  objects[1]:
-        return 0
-    if a == objects[2] and b == objects[0]:
-        return 1
-    if a == objects[2] and b == objects[1]:
-        return -1
-    if a == objects[2] and b == objects[2]:
-        return 0
 
-gameResult = rules(objects[char], second_player())
+rival_choice = second_player()
+
+gameResult = rules(objects[char], rival_choice)
+
+print("Your rival chose is " + rival_choice)
+
 if gameResult == 1:
     print("you're win")
 if gameResult == 0:
